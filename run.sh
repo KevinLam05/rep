16ccb467c40ce6e511d517e4fa7f4eaa913a69b0
KevinLam05 <107879311+KevinLam05@users.noreply.github.com>
2025-04-23 18:36:05 +0000

    m

--- run.sh
+++ run.sh
@@ -13,46 +13,22 @@
 #-----------------CODE---------------------------
 #!/bin/bash
 
-# Echo message to indicate the process has started
-echo "Assembling getqword.asm and manager.asm"
-
-# Assemble the getqword.asm and manager.asm files
+# Assembling getqword.asm and manager.asm
+echo "Assembling getqword.asm..."
 nasm -f elf64 -o getqword.o getqword.asm
+
+echo "Assembling manager.asm..."
 nasm -f elf64 -o manager.o manager.asm
 
-# Check if the assembly files were successfully assembled
-if [ $? -ne 0 ]; then
-    echo "Error assembling the ASM files."
-    exit 1
-fi
-
-# Echo message to indicate compilation of C++ file
-echo "Compiling wecome.cpp"
-
-# Compile the wecome.cpp C++ source file into an object file
+# Compiling wecome.cpp
+echo "Compiling wecome.cpp..."
 g++ -c wecome.cpp -o wecome.o
 
-# Check if the C++ file was compiled successfully
-if [ $? -ne 0 ]; then
-    echo "Error compiling the C++ file."
-    exit 1
-fi
+# Linking object files (disable PIE here)
+echo "Linking object files..."
+g++ -no-pie -o getqword.out wecome.o manager.o getqword.o
 
-# Echo message to indicate linking process
-echo "Linking object files"
-
-# Link the object files (getqword.o, manager.o, and wecome.o) into the final executable
-g++ -o getqword.out getqword.o manager.o wecome.o
-
-# Check if the linking was successful
-if [ $? -ne 0 ]; then
-    echo "Error linking the object files."
-    exit 1
-fi
-
-# Echo message to indicate that the program is running
-echo "Running program"
-
-# Run the program
+# Running program
+echo "Running program..."
 ./getqword.out
 
